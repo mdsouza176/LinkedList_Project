5d31a37c6f3637c763466a16b2d7e4931a9ba24a
Melroy Dsouza <melroydsouza176@gmail.com>
2018-10-21 17:04:04 -0700

    Update LinkedList.cpp

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -32,13 +32,48 @@ LinkedList::LinkedList(int value)
 // adds the two ints, and creates a new list which
 // carries one integer of the sum in everyone of its
 // nodes.
+// Constraints: unable to use any list function except access to nodes
+// data values and the next node. No going backwards.
 // precond: both Lists contain at least one node, 
 // the values in the nodes of the list create a reversed int
 // which will be added, ex. 3->4->5 = 345. The number of nodes does not exceed memory capacity.
 // postcond: returns a list with individual ints of the total sum in each node.
 ListNode * LinkedList::addTwoListNums(ListNode * l1, ListNode * l2)
 {
+	int carryOver = 0;
+	//The new list to be created.
+	ListNode *root;
+	//pointer that goes through new list when adding nodes.
+	ListNode *n = root;
+	while (l1 != nullptr || l2 != nullptr || carryOver != 0) {
+		int v1, v2, val = 0;
+		if (l1 != nullptr) {
+			v1 = l1->data;
+			l1 = l1->next;
+		}
+		if (l2 != nullptr) {
+			v2 = l2->data;
+			l2 = l2->next;
+		}
+		carryOver = (v1 + v2) / 10;
+		val = (v1 + v2) % 10;
+		n = new ListNode(val);
+		n = n->next;
+	}
 
+	return root;
+}
 
-	return nullptr;
+bool LinkedList::add(int value)
+{
+	if (headptr == nullptr) {
+		headptr = new ListNode(value);
+		return true;
+	}
+	ListNode *n = headptr;
+	while (n != nullptr) {
+		n = n->next;
+	}
+	n = new ListNode(value);
+	return true;
 }
